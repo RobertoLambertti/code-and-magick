369b1114cf072ea1b08141cfdc7feed427f9a10f
NikitaSech <NikitaSech@yandex.ru>
2020-05-31 15:22:20 +0300

    Рефакторинг

--- js/stat.js
+++ js/stat.js
@@ -11,11 +11,16 @@ var BAR_WIDTH = 40;
 var BAR_MAX_HEIGHT = 150;
 var BAR_GAP = 50;
 var BAR_GAP_TOP = 70;
-var BAR_YOU_COLOR = '#f00';
 
 var NAME_GAP_TOP = 260;
 
 var TIME_GAP_TOP = 50;
+
+var TITLE_RESULT = 'Ура вы победили!';
+var TITLE_LIST_RESULT = 'Список результатов:';
+
+var BLACK = '#000';
+var RED = '#f00';
 
 var maxTime;
 
@@ -31,12 +36,9 @@ function getRenderTitle(ctx, font, color, x, y, text) {
   ctx.fillText(text, x, y);
 }
 
-function getRandomNumber(min, max) {
-  return Math.floor(Math.random() * (max - min) + min);
-}
-
 function getRandomColorSaturation(h, l) {
-  return 'hsl(' + h + ',' + getRandomNumber(0, 100) + '%,' + l + '%';
+  var randomNumber = Math.floor(Math.random() * 100); // Записываем в переменную случайное целое число
+  return 'hsl(' + h + ',' + randomNumber + '%,' + l + '%'; // Выводим это число насыщенностью
 }
 
 function getMaxTime(times) {
@@ -49,12 +51,7 @@ function getPlayerName(ctx, player, i, color) {
 }
 
 function getPlayerChart(ctx, time, player, i) {
-  ctx.fillStyle = getRandomColorSaturation(240, 50); // Выбираем синего со случайной насыщенностью
-
-  if (player === 'Вы') {
-    ctx.fillStyle = BAR_YOU_COLOR; // Выбираем цвет из переменной
-  }
-
+  ctx.fillStyle = player === 'Вы' ? RED : getRandomColorSaturation(240, 50); // Выбираем цвет
   ctx.fillRect(CLOUD_X + GAP + (BAR_WIDTH + BAR_GAP) * (i - 1), CLOUD_Y + GAP + BAR_GAP_TOP + (BAR_MAX_HEIGHT - (BAR_MAX_HEIGHT * time / maxTime)), BAR_WIDTH, (BAR_MAX_HEIGHT * time) / maxTime);
 }
 
@@ -67,16 +64,17 @@ window.renderStatistics = function (ctx, players, times) {
   getRenderCloud(ctx, 'rgba(0, 0, 0, 0.7)', CLOUD_X + 10, CLOUD_Y + 10, CLOUD_WIDTH, CLOUD_HEIGHT);
   getRenderCloud(ctx, '#fff', CLOUD_X, CLOUD_Y, CLOUD_WIDTH, CLOUD_HEIGHT);
 
-  getRenderTitle(ctx, CLOUD_TEXT, '#000', CLOUD_X + GAP, CLOUD_Y + GAP, 'Ура вы победили!');
-  getRenderTitle(ctx, CLOUD_TEXT, '#000', CLOUD_X + GAP, CLOUD_Y + GAP * 2, 'Список результатов:');
+  getRenderTitle(ctx, CLOUD_TEXT, BLACK, CLOUD_X + GAP, CLOUD_Y + GAP, TITLE_RESULT);
+  getRenderTitle(ctx, CLOUD_TEXT, BLACK, CLOUD_X + GAP, CLOUD_Y + GAP * 2, TITLE_LIST_RESULT);
 
   maxTime = getMaxTime(times); // Записываем максимальное число в переменную
 
   for (var i = 1; i <= players.length; i++) {
-    times[i - 1] = Math.floor(times[i - 1]); // Округляем время до целого числа
+    var playerName = players[i - 1];
+    var playerTime = Math.floor(times[i - 1]); // Записываем в переменную время челым числом
 
-    getPlayerName(ctx, players[i - 1], i, 'black');
-    getPlayerTime(ctx, times[i - 1], i, 'black');
-    getPlayerChart(ctx, times[i - 1], players[i - 1], i);
+    getPlayerName(ctx, playerName, i, BLACK);
+    getPlayerTime(ctx, playerTime, i, BLACK);
+    getPlayerChart(ctx, playerTime, playerName, i);
   }
 };
