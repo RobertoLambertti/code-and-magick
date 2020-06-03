7c9418afad43b1bc48032cfba42f3dfa5f6a3ae0
NikitaSech <NikitaSech@yandex.ru>
2020-06-04 00:36:01 +0300

    Исправлены имена функция, массивов и переменных, добавлена функция генерации заданного кол-ва персонажей в массиве, функции генерации имени, плаща и глаз объеденены в одну

--- js/setup.js
+++ js/setup.js
@@ -1,68 +1,53 @@
 'use strict';
 
-var SIMILAR_WIZARD_TEMPLATE = document.querySelector('#similar-wizard-template')
+var similarWizardTemplate = document.querySelector('#similar-wizard-template')
   .content
   .querySelector('.setup-similar-item');
 
 var wizardsFragment = document.createDocumentFragment();
 
-var NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
-var SURNAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
-var COAT_COLOR = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
-var EYES_COLOR = ['black', 'red', 'blue', 'yellow', 'green'];
-
-var setup = {
-  ELEMENT: document.querySelector('.setup'),
-  ELEMENT_SIMULAR: document.querySelector('.setup-similar'),
-  LIST_ELEMENT: document.querySelector('.setup-similar-list'),
+var wizardsData = {
+  NAMES: ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'],
+  SURNAMES: ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'],
+  COAT_COLORS: ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'],
+  EYES_COLORS: ['black', 'red', 'blue', 'yellow', 'green'],
 };
 
-var wizards = [
-  {
-    name: getRandomName(), // Записываем результат генерации случайного имени
-    coatColor: getRandomCoatColor(), // Записываем результат генерации случайного света плаща
-    eyesColor: getRandomEyesColor(), // Записываем результат генерации глучайного цвет глаз
-  },
-  {
-    name: getRandomName(),
-    coatColor: getRandomCoatColor(),
-    eyesColor: getRandomEyesColor(),
-  },
-  {
-    name: getRandomName(),
-    coatColor: getRandomCoatColor(),
-    eyesColor: getRandomEyesColor(),
-  },
-  {
-    name: getRandomName(),
-    coatColor: getRandomCoatColor(),
-    eyesColor: getRandomEyesColor(),
-  },
-];
+var setup = {
+  element: document.querySelector('.setup'),
+  similarElement: document.querySelector('.setup-similar'),
+  listElement: document.querySelector('.setup-similar-list'),
+};
+
+var wizards = [];
 
 function getRandomNumber(min, max) {
   return Math.floor(Math.random() * (max - min) + min);
 }
 
-function getRandomName() {
-  var randomName = NAMES[getRandomNumber(0, NAMES.length)]; // Случайно выбираем имя из массива и записываем в переменную
-  var randomSurname = SURNAMES[getRandomNumber(0, SURNAMES.length)]; // Случайно выбираем фамилию из массива и записываем в переменную
-
-  return randomName + ' ' + randomSurname; // Возвращаем комбинацию имени и фамилии
+function getArrayWizards(quantity) {
+  for (var i = 0; i < quantity; i++) {
+    wizards[i] = {}; // Создаём пустые массивы
+  }
 }
 
-function getRandomCoatColor() {
-  var randomCoatColor = COAT_COLOR[getRandomNumber(0, COAT_COLOR.length)]; // Случайно выбираем цвет плаща из массива и записываем в переменную
-  return randomCoatColor; // Возвращаем цвет плаща
+function getRandomWizards() {
+  for (var i = 0; i < wizards.length; i++) {
+    var randomName = wizardsData.NAMES[getRandomNumber(0, wizardsData.NAMES.length)]; // Случайно выбираем имя из массива и записываем в переменную
+    var randomSurname = wizardsData.SURNAMES[getRandomNumber(0, wizardsData.SURNAMES.length)]; // Случайно выбираем фамилию из массива и записываем в переменную
+    var randomCoatColor = wizardsData.COAT_COLORS[getRandomNumber(0, wizardsData.COAT_COLORS.length)]; // Случайно выбираем цвет плаща из массива и записываем в переменную
+    var randomEyesColor = wizardsData.EYES_COLORS[getRandomNumber(0, wizardsData.EYES_COLORS.length)]; // Случайно выбираем цвет глаз из массива и записываем в переменную
+
+    wizards[i] = {
+      name: randomName + ' ' + randomSurname, // Создаём ключ и записываем под ним результат генерации случайного имени
+      coatColor: randomCoatColor, // Создаём ключ и записываем под ним результат генерации случайного цвета плаща
+      eyesColor: randomEyesColor, // Создаём ключ и записываем под ним результат генерации случайного цвета глаз
+    };
+  }
 }
 
-function getRandomEyesColor() {
-  var randomEyesColor = EYES_COLOR[getRandomNumber(0, EYES_COLOR.length)]; // Случайно выбираем цвет глаз из массива и записываем в переменную
-  return randomEyesColor; // Возвращаем цвет глаз
-}
-
-function getRenderWizard(wizard) {
-  var wizardElement = SIMILAR_WIZARD_TEMPLATE.cloneNode(true); // Кнонируем узел со всем содержимым и записываем в переменную
+function getCreateWizard(wizard) {
+  var wizardElement = similarWizardTemplate.cloneNode(true); // Кнонируем узел со всем содержимым и записываем в переменную
   var wizardNameElement = wizardElement.querySelector('.setup-similar-label'); // Находим из клона елемент отвечающий за имя и записываем в переменную
   var wizardCoatColorElement = wizardElement.querySelector('.wizard-coat'); // Находим из клона елемент отвечающий за цвет плаща и записываем в переменную
   var wizardEyesColorElement = wizardElement.querySelector('.wizard-eyes'); // Находим из клона елемент отвечающий за цвет глаз и записываем в переменную
@@ -74,11 +59,14 @@ function getRenderWizard(wizard) {
   return wizardElement; // Возвращаем одного персонажа
 }
 
-setup.ELEMENT.classList.remove('hidden'); // Показываем попап редактирования персонажа
+setup.element.classList.remove('hidden'); // Показываем попап редактирования персонажа
+
+getArrayWizards(9); // Генерируем массив из четырёх персонажей
+getRandomWizards(); // Генерируем случайные данные для персонажей
 
 for (var wizard = 0; wizard < wizards.length; wizard++) {
-  wizardsFragment.appendChild(getRenderWizard(wizard)); // Вставляем в конец фрагмента одного персонажа на каждой итерации
+  wizardsFragment.appendChild(getCreateWizard(wizard)); // Вставляем в конец фрагмента одного персонажа на каждой итерации
 }
 
-setup.LIST_ELEMENT.appendChild(wizardsFragment); // Вставляем фрагмент в разметку
-setup.ELEMENT_SIMULAR.classList.remove('hidden'); // Показываем блок с похожими персонажами
+setup.listElement.appendChild(wizardsFragment); // Вставляем фрагмент в разметку
+setup.similarElement.classList.remove('hidden'); // Показываем блок с похожими персонажами
